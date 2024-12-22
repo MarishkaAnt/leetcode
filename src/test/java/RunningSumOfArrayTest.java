b746de0f00287ab4ac92d33611c8ada7e57a9059
Mariia Antonova <mariia_antonova@epam.com>
2024-12-22 11:51:46 +0300

    RunningSumOfArrayTest add comment for test cases

--- src/test/java/RunningSumOfArrayTest.java
+++ src/test/java/RunningSumOfArrayTest.java
@@ -1,6 +1,16 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/*
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ */
 class RunningSumOfArrayTest {
 
     @Test
