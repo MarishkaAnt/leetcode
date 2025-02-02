3940d00959a2909cdbacc08882d5521a7adf2bb3
Mariia Antonova <mariia_antonova@epam.com>
2025-02-02 14:30:05 +0300

    1768. Merge Strings Alternately

---
+++ src/main/java/MergeStringsAlternately.java
@@ -0,0 +1,40 @@
+/**
+ * 1768. Merge Strings Alternately
+ */
+/*
+You are given two strings word1 and word2.
+Merge the strings by adding letters in alternating order, starting with word1.
+If a string is longer than the other, append the additional letters onto the end of the merged string.
+Return the merged string.
+
+Example 1:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+
+Example 2:
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+
+Example 3:
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+ */
+public class MergeStringsAlternately {
+    public static String mergeAlternately(String word1, String word2) {
+
+        StringBuilder sb = new StringBuilder();
+        int maxLength = Math.max(word1.length(), word2.length());
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < word1.length()) {
+                sb.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                sb.append(word2.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    }
+
+}
