315e92ebbd2b84aede1816460a8713747b2972fc
Mariia Antonova <mariia_antonova@epam.com>
2025-02-03 23:25:55 +0300

    1431. Kids With the Greatest Number of Candies

---
+++ src/main/java/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1431. Kids With the Greatest Number of Candies
+ */
+/*
+There are n kids with candies. You are given an integer array candies,
+where each candies[i] represents the number of candies the ith kid has,
+and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if,
+after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids,
+or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.
+
+Example 2:
+Input: candies = [4,2,1,1,2], extraCandies = 1
+Output: [true,false,false,false,false]
+Explanation: There is only 1 extra candy.
+Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+
+Example 3:
+Input: candies = [12,1,12], extraCandies = 10
+Output: [true,false,true]
+ */
+public class KidsWithTheGreatestNumberOfCandies {
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        List<Boolean> result = new ArrayList<>();
+        int maxCandies = 0;
+        for (int candiesCount : candies) {
+            if (candiesCount > maxCandies) {
+                maxCandies = candiesCount;
+            }
+        }
+
+        for (int candy : candies) {
+            if (candy + extraCandies >= maxCandies) {
+                result.add(Boolean.TRUE);
+            } else {
+                result.add(Boolean.FALSE);
+            }
+        }
+
+        return result;
+    }
+}
